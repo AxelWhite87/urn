bb4cad97beefc8c86dd2d1fb428753c7a7dd3403
AxelWhite87 <alexandrecwhitehouse@gmail.com>
2023-01-30 21:08:04 -0500

    Missed a line

--- urn/script.py
+++ urn/script.py
@@ -3,6 +3,6 @@ from database import *
 cli = CliContext()
 
 with db_session as session:
-    cli.add_game(name="Legend of the Five Rings")
+    cli.game_context = cli.add_game(name="Legend of the Five Rings")
     cli.add_fact(text="I am a test", game=Game.get(name="Legend of the Five Rings"))
     cli.delete_fact(1)
