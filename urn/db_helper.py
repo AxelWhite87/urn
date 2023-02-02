ef144e31f12fc00cbe07d24c2af3087a3ad4bdd8
Alexandre Whitehouse <awhitehouse@pcc.com>
2023-02-02 14:36:05 -0600

    Added DB helper file for scoping

---
+++ urn/db_helper.py
@@ -0,0 +1,47 @@
+# File that holds all DB helper functions
+# Also serves as a manifest of what we can do
+
+# Create a new game
+def create_game(name: str):
+    pass
+
+
+# List all your games
+# Shows name and ID
+def list_games():
+    pass
+
+
+# Switch to a game
+# Requires the ID
+def switch_game(id: int):
+    pass
+
+
+# Delete a game
+# This will delete ALL items linked to this game
+def delete_game(id: int):
+    pass
+
+
+## All the following functions are based on the game that's been selected ##
+
+# Show all facts and open tasks linked to this game
+def show():
+    pass
+
+
+# Show all facts
+def show_facts():
+    pass
+
+
+# Show all information for one fact
+def show_fact(id: int):
+    pass
+
+
+# Add a fact
+def add_fact(text: str):
+    pass
+
